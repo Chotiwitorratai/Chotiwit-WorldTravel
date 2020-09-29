4de37939662cf074abceafaffd0f43a2c3f958af
Chotiwit <chotiwit1223@hotmail.com>
2020-09-29 13:23:07 +0700

    first commit4

--- inventory/settings.py
+++ inventory/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stockdb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST' : 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
